3c282fc5b1dc33eaca3130e082804de1c06e90e3
JuiceWRRLD <999shiiz@gmail.com>
2025-07-09 06:17:27 -0300

    feat: usar Pixeldrain con auth para videos grandes

--- utils/pixeldrain.py
+++ utils/pixeldrain.py
@@ -1,10 +1,19 @@
 import httpx
+from httpx import BasicAuth
+
+API_KEY = "600de034-beea-4ae0-8986-aaab371b8bc7"
 
 def upload(file_path):
     try:
         with open(file_path, "rb") as f:
-            files = {'file': (file_path, f, 'application/octet-stream')}
-            response = httpx.post("https://pixeldrain.com/api/file", files=files, timeout=60.0)
+            files = {"file": (file_path, f, "application/octet-stream")}
+            auth = BasicAuth("", API_KEY)
+            response = httpx.post(
+                "https://pixeldrain.com/api/file",
+                files=files,
+                auth=auth,
+                timeout=60.0
+            )
 
         if response.status_code == 200:
             data = response.json()
